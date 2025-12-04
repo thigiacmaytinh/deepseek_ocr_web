6bcaff22cff16912377696ad38ebe37986deec02
vohungvi <vohungvi27@gmail.com>
2025-12-04 19:47:23 +0700

    - active env

---
+++ backend/start_backend.bat
@@ -0,0 +1,2 @@
+call venv\Scripts\activate
+python -m uvicorn main:app --host 0.0.0.0 --port 8000
--- start_backend.bat
+++ start_backend.bat
@@ -1,2 +1,2 @@
 cd backend
-python -m uvicorn main:app --host 0.0.0.0 --port 8000
+start_backend.bat
